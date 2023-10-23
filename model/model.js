29132ff8d858eb2316c9855cea28f3a83600cc01
rc-balaji <balajic@drngpit.ac.in>
2023-10-23 20:19:10 +0530

    aaa

--- model/model.js
+++ model/model.js
@@ -4,6 +4,8 @@ const Auth = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  dob: { type: String },
+  age: { type: String },
 });
 
 module.exports = mongoose.model("Auths", Auth);
--- router/router.js
+++ router/router.js
@@ -3,8 +3,34 @@ const model = require("../model/model");
 
 const router = express.Router();
 
+router.put("/updateProfile", async (req, res) => {
+  const { username, dob, age } = req.body;
+
+  try {
+    // Find the user by their username
+    const user = await model.findOne({ username: username });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update the user's profile data
+    user.dob = dob;
+    user.age = age;
+
+    // Save the updated user data
+    await user.save();
+
+    return res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.error("Failed to update profile:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Endpoint to register a user
 router.post("/register", async (req, res) => {
-  const { uname, mail, password } = req.body;
+  const { uname, mail, password, dob, age } = req.body;
   try {
     const existingUser = await model.findOne({ email: mail });
 
@@ -15,6 +41,8 @@ router.post("/register", async (req, res) => {
         username: uname,
         email: mail,
         password: password,
+        dob: dob || null, // Set to null if not provided
+        age: age || null, // Set to null if not provided
       });
 
       await newUser.save();
